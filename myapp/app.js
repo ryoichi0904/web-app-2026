0fd9779975145a563c45cd5c62e7424159900a2b
ryouichi takamatsu <nk250056@nks.iput.ac.jp>
2026-06-01 11:59:18 +0900

    ポート番号を環境変数から読むように変更

--- myapp/app.js
+++ myapp/app.js
@@ -1,10 +1,75 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
+const { Pool } = require('pg');
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+
+const pool = new Pool({
+  host:     process.env.DB_HOST,
+  port:     process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
-app.listen(3000, () => {
-  console.log('サーバーが起動しました: http://localhost:3000');
+
+app.get('/', (req, res) => {
+  res.send('トップページです');
+});
+
+app.get('/about', (req, res) => {
+  res.send('自己紹介ページです');
+});
+
+app.get('/time', (req, res) => {
+  const now = new Date().toLocaleString('ja-JP');
+  res.send(`現在の時刻: ${now}`);
+});
+
+app.get('/api/test',(req,res) => {
+  res.json({message: 'APIが動いてます', status:'ok'})
+});
+
+app.get('/status', (req, res) => {
+  res.json({ status: 'ok' , message: 'サーバーは正常に動作しています' });
+});
+
+const messages = [];
+
+app.get("/api/messages",async (req, res) => {
+  const result = await pool.query(
+    'SELECT * FROM messages ORDER BY created_at ASC'
+  );
+  res.json(result.rows);
+
+});
+
+app.post("/api/messages",async (req, res) => {
+  //const { username, text } = req.body;
+  //const newMessage = { id: messages.length + 1, username, text };
+  //messages.push(newMessage);
+  // res.json(newMessage);
+   const { username, text } = req.body;
+  const result = await pool.query(
+    'INSERT INTO messages (username, text) VALUES ($1, $2) RETURNING *',
+    [username, text]
+  );
+  res.json(result.rows[0]);
+});
+
+app.post('/api/messages',(req,res) => {
+  const{username,text} = req.body;
+  const newItem = {username,text};
+  console.log(newItem);
+  res.json(newItem);
+
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `サーバが起動しました： http://localhost:${process.env.PORT || 3000}`,
+  );
 });
